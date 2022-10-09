acbb497cd20b24f15c9704c938d601f064aeb0cb
Carlos Gonzalez <gonzalez.carlos@externos-cl.cencosud.com>
2022-10-09 19:27:03 -0300

    Tailwind's dark mode properly implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      accent: "red",
+      accentDark: "blue",
+    },
     extend: {},
   },
   plugins: [],
